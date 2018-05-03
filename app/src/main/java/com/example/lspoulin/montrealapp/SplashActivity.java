9e5aeaf48316706ee9713825eeddb130eaef8e43
Unknown <lspoulin@gmail.com>
2018-05-03 11:55:12 -0400

    Ajout d'un splash screen et d'icon

--- app/src/main/java/com/example/lspoulin/montrealapp/SplashActivity.java
+++ app/src/main/java/com/example/lspoulin/montrealapp/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.example.lspoulin.montrealapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,18 +13,9 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 }
